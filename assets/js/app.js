1e6887190f52d1841f8bd35730a68eb99b553782
Ignacio Prados <ignacioprados.ip@gmail.com>
2020-12-28 21:17:21 -0300

    Publico Página COCINA CONMIGO

---
+++ assets/js/app.js
@@ -0,0 +1,114 @@
+/* ===== IGNACIO PRADOS ===== */
+/* ===== IGNACIO PRADOS ===== */
+/* ===== IGNACIO PRADOS ===== */
+
+
+
+
+
+
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+/*Active link*/
+navLink.forEach(n => n.classList.remove('active'));
+this.classList.add('active');
+
+/*Remove menu mobile*/
+const navMenu = document.getElementById('nav-menu')
+navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.navegacion',{});
+sr.reveal('#logotipo',{delay: 200});
+sr.reveal('',{delay: 400});
+sr.reveal('.home__social-icon',{ interval: 200});
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{});
+sr.reveal('.about__subtitle',{delay: 400});
+sr.reveal('.about__text',{delay: 400});
+
+
+
+
+/* RECETAS POR CATEGORÍA */
+
+$(document).ready(function(){
+
+	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
+
+	// FILTRANDO PRODUCTOS  ============================================
+
+	$('.category_item').click(function(){
+		var catProduct = $(this).attr('category');
+		console.log(catProduct);
+
+		// AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
+		$('.category_item').removeClass('ct_item-active');
+		$(this).addClass('ct_item-active');
+
+		// OCULTANDO PRODUCTOS =========================
+		$('.product-item').css('transform', 'scale(0)');
+		function hideProduct(){
+			$('.product-item').hide();
+		} setTimeout(hideProduct,400);
+
+		// MOSTRANDO PRODUCTOS =========================
+		function showProduct(){
+			$('.product-item[category="'+catProduct+'"]').show();
+			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
+		} setTimeout(showProduct,400);
+	});
+
+	// MOSTRANDO TODOS LOS PRODUCTOS =======================
+
+	$('.category_item[category="all"]').click(function(){
+		function showAll(){
+			$('.product-item').show();
+			$('.product-item').css('transform', 'scale(1)');
+		} setTimeout(showAll,400);
+	});
+
+
+
+});
+
+/*SLIDER*/
+
+
+
+
+
+
+
+
+
+/* ===== IGNACIO PRADOS ===== */
+/* ===== IGNACIO PRADOS ===== */
+/* ===== IGNACIO PRADOS ===== */
